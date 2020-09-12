fec7e79728c48f13b899baeed12a0a360c3ed680
MJ <mjstthomas9516@gmail.com>
2020-09-12 08:09:03 -0700

    finished large button

---
+++ src/Components/LargeButton/LargeButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './LargeButton.css';
+
+export default function LargeButton(props){
+    return (
+        <button className="large-btn" onClick={props.onClick}>{props.name}</button>
+    )
+}
