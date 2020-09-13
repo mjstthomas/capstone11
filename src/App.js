a7ebfb3ee2f86578fc47a09f0798dc1711c75104
Jamie Tater <jtater1@gmail.com>
2020-09-13 18:24:15 -0400

    added functions for skills and started search page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 // import LandingPage from './Routes/LandingPage';
 // import SignUp from './Routes/signup';
-import Login from './Routes/login';
+import Login from "./Routes/login";
 // import BizDash from './Routes/Business/BizDash';
-// import Search from './Routes/Business/Search';
+import Search from "./Routes/Business/Search/Search";
 // import Results from './Routes/Business/Results';
 // import FLProfile from './Routes/Business/FLProfile';
 // import MakeOffer from './Routes/Business/MakeOffer';
@@ -13,32 +13,71 @@ import Login from './Routes/login';
 // import OffersPage from './Routes/Freelance/OffersPage';
 // import Offer from './Routes/Freelance/Offer';
 
-import AppContext from './AppContext';
-import './App.css';
+import AppContext from "./AppContext";
+import "./App.css";
 
 class App extends React.Component {
-  state = {};
-  render(props){
+  state = {
+    MustHaveSkills: [{ level: "", skill: "" }],
+    NiceToHaveSkills: [{ level: "", skill: "" }],
+  };
+  setLevel = (level, index, typeOfSkill) => {
+    const prevState = this.state;
+    prevState[typeOfSkill][index].level = level;
+    this.setState(prevState);
+  };
+  setSkill = (skill, index, typeOfSkill) => {
+    const prevState = this.state;
+    prevState[typeOfSkill][index].skill = skill;
+    this.setState(prevState);
+  };
+  addSkill = (e) => {
+    let typeOfSkill = e.target.parentNode.parentNode.parentNode.childNodes[0].childNodes[0].innerHTML
+      .split(" ")
+      .join("");
+    typeOfSkill = typeOfSkill + "Skills";
+    const prevState = this.state;
+    prevState[typeOfSkill].push({ level: "", skill: "" });
+    this.setState(prevState);
+  };
+  removeSkill = (index, typeOfSkill) => {
+    const prevState = this.state;
+    prevState[typeOfSkill].splice(index, 1);
+    this.setState(prevState);
+  };
+
+  render(props) {
     let context = {};
-  return (
-    <AppContext.Provider value={context}>
-      <div className="App">
-        {/* {<Route path='/' exact component={LandingPage} />
+    return (
+      <AppContext.Provider value={context}>
+        <div className="App">
+          {/* {<Route path='/' exact component={LandingPage} />
         <Route path='/SignUp' exact component={SignUp} />} */}
-        {<Route path='/Login' exact component={Login} />}
-        {/* {<Route path='/Business' exact component={BizDash} />
-        <Route path='/Business/Search' exact component={Search} />
-        <Route path='/Business/Results' exact component={Results} />
+          {<Route path="/Login" exact component={Login} />}
+          {/* <Route path='/Business' exact component={BizDash} /> */}
+          <Route
+            path="/Business/Search"
+            render={(props) => (
+              <Search
+                MustHaveSkills={this.state.MustHaveSkills}
+                NiceToHaveSkills={this.state.NiceToHaveSkills}
+                setLevel={this.setLevel}
+                setSkill={this.setSkill}
+                addSkill={this.addSkill}
+                removeSkill={this.removeSkill}
+              />
+            )}
+          />
+          {/* <Route path='/Business/Results' exact component={Results} />
         <Route path='/Business/Results/:freelanceID' exact component={FLProfile} />
         <Route path='/Business/Results/:freelanceID/MakeOffer' exact component={MakeOffer} />
         <Route path='/Business/BizOffersDash' exact component={BizOffersDash} />
         <Route path='/Freelancer' exact component={FreelanceDash} />
         <Route path='/Freelancer/OffersPage' exact component={OffersPage} />
-        <Route path='/Freelancer/OffersPage/:offerId' exact component={Offer} />} */}
-
-      </div>
-    </AppContext.Provider>
-    )
+        <Route path='/Freelancer/OffersPage/:offerId' exact component={Offer} /> */}
+        </div>
+      </AppContext.Provider>
+    );
   }
 }
 
--- src/Components/Utilities/AddSkill/AddSkill.js
+++ src/Components/Utilities/AddSkill/AddSkill.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cancel from "../DenyButton/DenyButton";
 import "./AddSkill.css";
 
 function getOptions() {
@@ -13,14 +14,20 @@ function getOptions() {
   return options;
 }
 
-function AddSkill() {
+function AddSkill(props) {
   const options = getOptions();
 
   return (
-    <article>
+    <article className="skill-input">
       <section className="container">
-        <label htmlFor="skill-level">Skill Level </label>
-        <select id="skill-level" name="skill-level">
+        <label htmlFor="skill-level">Experience</label>
+        <select
+          onChange={(e) =>
+            props.setLevel(e.target.value, props.index, props.typeOfSkill)
+          }
+          id="skill-level"
+          name="skill-level"
+        >
           <option default>---</option>
           <option>Entry Level</option>
           <option>Intermediate</option>
@@ -29,15 +36,24 @@ function AddSkill() {
       </section>
       <span> in </span>
       <section className="container">
-        <label htmlFor="skill-name">Skill </label>
+        <label htmlFor="skill-name">Skill</label>
         <input
           type="text"
           id="skill-name"
           name="skill-name"
           list="skill-list"
+          onChange={(e) =>
+            props.setSkill(e.target.value, props.index, props.typeOfSkill)
+          }
         />
         <datalist id="skill-list">{options}</datalist>
       </section>
+      <Cancel
+        onClick={(e) => {
+          e.preventDefault();
+          props.removeSkill(props.index, props.typeOfSkill);
+        }}
+      />
     </article>
   );
 }
