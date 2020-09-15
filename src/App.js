67bc52fecf335e25742d963493619129433e8f83
MJ <mjstthomas9516@gmail.com>
2020-09-15 16:19:10 -0700

    made changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { Route } from "react-router-dom";
 // import LandingPage from './Routes/LandingPage';
 import SignUp from './Routes/signup';
 import Login from "./Routes/login";
-import FLDetsForm from "./Routes/Freelance/FLProfileDetsForm/FLDetsForm";
-// import BizDash from './Routes/Business/BizDash';
+import BizDash from './Routes/Business/BizDash/BizDash';
 import Search from "./Routes/Business/Search/Search";
 // import Results from './Routes/Business/Results';
 // import FLProfile from './Routes/Business/FLProfile';
@@ -18,38 +17,9 @@ import AppContext from "./AppContext";
 import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.setLevel = (level, index, typeOfSkill) => {
-      const prevState = this.state;
-      prevState[typeOfSkill][index].level = level;
-      this.setState(prevState);
-    };
-    this.setSkill = (skill, index, typeOfSkill) => {
-      const prevState = this.state;
-      prevState[typeOfSkill][index].skill = skill;
-      this.setState(prevState);
-    };
-    this.addSkills = (e) => {
-      let typeOfSkill = e.target.parentNode.parentNode.parentNode.childNodes[0].childNodes[0].innerHTML
-        .split(" ")
-        .join("");
-      const prevState = this.state;
-      prevState[typeOfSkill].push({ level: "", skill: "" });
-      this.setState(prevState);
-    };
-    this.removeSkill = (index, typeOfSkill) => {
-      const prevState = this.state;
-      prevState[typeOfSkill].splice(index, 1);
-      this.setState(prevState);
-    };
-  }
-
   state = {
-    AddSkills: [{ level: "", skill: "" }],
     MustHaveSkills: [{ level: "", skill: "" }],
     NiceToHaveSkills: [{ level: "", skill: "" }],
-<<<<<<< HEAD
   };
 
   setLevel = (level, index, typeOfSkill) => {
@@ -78,19 +48,12 @@ class App extends React.Component {
     const prevState = this.state;
     prevState[typeOfSkill].splice(index, 1);
     this.setState(prevState);
-=======
-    setLevel: this.setLevel,
-    setSkill: this.setSkill,
-    addSkills: this.addSkills,
-    removeSkill: this.removeSkill,
->>>>>>> 872be9692999f424f0020959cd13d37ff3f56d07
   };
 
   testContext = () =>{
     console.log('well it worked')
   }
   render(props) {
-<<<<<<< HEAD
     let context = {
       testContext: this.testContext,
       removeSkill: this.removeSkill,
@@ -98,18 +61,27 @@ class App extends React.Component {
       setSkill: this.setSkill,
       setLevel: this.setLevel
     };
-=======
-    let context = this.state;
->>>>>>> 872be9692999f424f0020959cd13d37ff3f56d07
+    
     return (
       <AppContext.Provider value={context}>
         <div className="App">
-          {/* {<Route path='/' exact component={LandingPage} />
-        <Route path='/SignUp' exact component={SignUp} />} */}
-          {<Route path="/Login" exact component={Login} />}
-          <Route path="/SignUp/FLDetails" component={FLDetsForm} />
-          {/* <Route path='/Business' exact component={BizDash} /> */}
-          <Route path="/Business/Search" component={Search} />
+        {/* {<Route path='/' exact component={LandingPage} />} */}
+          <Route path='/SignUp' exact component={SignUp} /> 
+          <Route path="/Login" exact component={Login} />
+          <Route path='/Business' exact component={BizDash} />
+          <Route
+            path="/Business/Search"
+            render={(props) => (
+              <Search
+                MustHaveSkills={this.state.MustHaveSkills}
+                NiceToHaveSkills={this.state.NiceToHaveSkills}
+                setLevel={this.setLevel}
+                setSkill={this.setSkill}
+                addSkill={this.addSkill}
+                removeSkill={this.removeSkill}
+              />
+            )}
+          />
           {/* <Route path='/Business/Results' exact component={Results} />
         <Route path='/Business/Results/:freelanceID' exact component={FLProfile} />
         <Route path='/Business/Results/:freelanceID/MakeOffer' exact component={MakeOffer} />
