55e342bc55d0d21356ec0e30ff0b35685ad5ab39
Jamie Tater <jtater1@gmail.com>
2020-09-16 00:24:43 -0400

    made adjustments to landing page and small button

--- src/Components/Utilities/SmallButton/SmallButton.js
+++ src/Components/Utilities/SmallButton/SmallButton.js
@@ -14,15 +14,13 @@ const SmallButton = ({ children, type, onClick, buttonStyle, buttonSize }) => {
   const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
   return (
-    <Link to="/sign-up" className="btn-mobile">
-      <button
-        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
-        onClick={onClick}
-        type={type}
-      >
-        {children}
-      </button>
-    </Link>
+    <button
+      className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+      onClick={onClick}
+      type={type}
+    >
+      {children}
+    </button>
   );
 };
 
