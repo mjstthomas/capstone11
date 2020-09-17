60f1c84448e0c430aedaa704343b81d179ea6572
Tuan Nguyen <tnpm7@mst.edu>
2020-09-17 04:43:14 -0600

    initial offer pages

---
+++ src/Routes/Business/BusinessOffersPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import BCards from "../Cards/BCards";
+import Header from "../Components/Header/Header";
+
+function BusinessOffersPage() {
+  return (
+    <>
+      <Header />
+      <BCards />
+    </>
+  );
+}
+
+export default BusinessOffersPage;
---
+++ src/Routes/Freelance/FreelanceOffersPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import FCards from "../Cards/FCards";
+import Header from "../Components/Header/Header";
+
+function FreelanceOffersPage() {
+  return (
+    <>
+      <Header />
+      <FCards />
+    </>
+  );
+}
+
+export default FreelanceOffersPage;
