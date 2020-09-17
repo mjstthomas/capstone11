1a29c2e1798d312d934e9470582f11b114c3db2b
Tuan Nguyen <tnpm7@mst.edu>
2020-09-17 03:56:33 -0600

    Initial offer cards

---
+++ src/Routes/Cards/BusinessCardItem.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function BusinessCardItem(props) {
+  return (
+    <>
+      <li className="cards-item">
+        <Link className="cards-item-link" to={props.path}>
+          <img className="cards-item-img" alt="Profile Image" src={props.src} />
+        </Link>
+        <div className="cards-item-info">
+          <h2 className="cards-item-text">{props.name}</h2>
+          <h2 className="cards-item-text">{props.rating}</h2>
+          <h5 className="cards-item-text">{props.date}</h5>
+          <h5 className="cards-item-text">{props.pay}</h5>
+          <h5 className="cards-item-text">{props.text}</h5>
+        </div>
+        <Link ClassName="message-bubble" to={props.path}>
+          <img
+            className="cards-item-message"
+            alt="Message Bubble"
+            src={props.imgSrc}
+          />
+        </Link>
+      </li>
+    </>
+  );
+}
+
+export default BusinessCardItem;
