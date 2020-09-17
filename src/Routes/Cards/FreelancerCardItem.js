de96895a7b938f8a83d0e3521b4f272aab77b182
Tuan Nguyen <tnpm7@mst.edu>
2020-09-17 04:01:17 -0600

    Changed initial offer cards

--- src/Routes/Cards/FreelancerCardItem.js
+++ src/Routes/Cards/FreelancerCardItem.js
@@ -6,24 +6,29 @@ function BusinessCardItem(props) {
     <>
       <li className="cards-item">
         <Link className="cards-item-link" to={props.path}>
-          <img className="cards-item-img" alt="Profile Image" src={props.src} />
-        </Link>
-        <div className="cards-item-info">
-          <h2 className="cards-item-text">{props.name}</h2>
-          <h2 className="cards-item-text">{props.rating}</h2>
-          <h5 className="cards-item-text">{props.date}</h5>
-          <h5 className="cards-item-text">{props.pay}</h5>
-          <h5 className="cards-item-text">{props.text}</h5>
-        </div>
-        <Link ClassName="message-bubble" to={props.path}>
-          <img
-            className="cards-item-message"
-            alt="Message Bubble"
-            src={props.imgSrc}
-          />
-        </Link>
-        <Link className="offer-bubble" type="submit">
-          Accept Offer
+          <figure className="cards-item-pic-wrap">
+            <img
+              className="cards-item-img"
+              alt="Profile Image"
+              src={props.src}
+            />
+          </figure>
+          <div className="cards-item-info">
+            <h2 className="cards-item-text">{props.name}</h2>
+            <h2 className="cards-item-text">{props.rating}</h2>
+            <h5 className="cards-item-text">{props.pay}</h5>
+            <h5 className="cards-item-text">{props.text}</h5>
+          </div>
+          <Link ClassName="message-bubble" to={props.path}>
+            <img
+              className="cards-item-message"
+              alt="Message Bubble"
+              src={props.imgSrc}
+            />
+          </Link>
+          <Link className="offer-bubble" type="submit">
+            Accept Offer
+          </Link>
         </Link>
       </li>
     </>
