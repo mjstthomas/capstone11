55af16916b1da0b3433e8cad555f15263b1bc0a5
Tuan Nguyen <tnpm7@mst.edu>
2020-09-19 05:36:37 -0600

    edited make offer home

--- src/Routes/Business/MakeOffer/MakeOfferHome.js
+++ src/Routes/Business/MakeOffer/MakeOfferHome.js
@@ -1,9 +1,11 @@
 import React from "react";
 import MakeOffer from "./MakeOffer";
+import Header from "../Components/Header/Header";
 
 function MakeOfferHome() {
   return (
     <>
+      <Header />
       <MakeOffer />
     </>
   );
