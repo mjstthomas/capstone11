0986fd6f578583ac5112dc204e05d299525bc0d6
MJ <mjstthomas9516@gmail.com>
2020-09-20 00:59:54 -0700

    added functionality up to makeoffer

--- src/Routes/Business/Results/Results.js
+++ src/Routes/Business/Results/Results.js
@@ -36,6 +36,8 @@ export default function Results(props) {
     console.log(item);
     return (
       <ResultCard
+        id={item.id}
+        key={item.id}
         name={item.nickname}
         skills={item.skills}
         rating={item.rating}
--- src/Routes/Business/Results/components/ResultCard.js
+++ src/Routes/Business/Results/components/ResultCard.js
@@ -1,25 +1,28 @@
 import React from "react";
 import "./ResultCard.css";
+import { Link } from 'react-router-dom';
 import ninja from "../../../../imgs/ninja.png";
 
 export default function ResultCard(props) {
   const skill =
     props.skills.map((skill) => <p className="skill">{skill}</p>) || "";
   return (
-    <section className="result-card-container">
-      <section className="result-card">
-        <h5 className="result-card-header">{props.name}</h5>
+    <Link to={`/Business/Results/${props.id}`}>
+        <section className="result-card-container">
+        <section className="result-card">
+            <h5 className="result-card-header">{props.name}</h5>
 
-        <article className="result-card-image-container">
-          <img className="result-image" src={ninja} alt="result" />
-        </article>
-        <article className="result-card-rating">
-          <p>
-            Rating: <span className="rating">{props.rating}</span>
-          </p>
-        </article>
-        <article className="skills">{skill}</article>
-      </section>
-    </section>
+            <article className="result-card-image-container">
+            <img className="result-image" src={ninja} alt="result" />
+            </article>
+            <article className="result-card-rating">
+            <p>
+                Rating: <span className="rating">{props.rating}</span>
+            </p>
+            </article>
+            <article className="skills">{skill}</article>
+        </section>
+        </section>
+    </Link>
   );
 }
