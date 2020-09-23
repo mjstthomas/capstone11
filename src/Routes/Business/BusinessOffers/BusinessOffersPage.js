5cb35c8633b26ebf8afd850b14a7db8ad61ddd1b
Tngtater9 <jtater1@gmail.com>
2020-09-22 20:04:11 -0400

    finished business cards and made responsive

--- src/Routes/Business/BusinessOffers/BusinessOffersPage.js
+++ src/Routes/Business/BusinessOffers/BusinessOffersPage.js
@@ -1,32 +1,78 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import BusinessCardItem from "./Components/BusinessCardItem";
 import Header from "../../../Components/Header/Header";
 import AppContext from "../../../AppContext";
+import "./BusinessOffersPage.css";
 
 function BusinessOffersPage(props) {
   const context = useContext(AppContext);
-  const myOffers = context.businessOffers.filter(
-    (item) => item.businessID == context.user.id
-  );
 
-  const offers = myOffers.map(item =>{
-    return <BusinessCardItem
-              date="09/03/20"
-              pay={item.pay}
-              text={item.text}
-              freelancerID={item.userID}
-              businessID={item.businessID}
-              history={props.history}
-            />
-  })
+  const myOffers = [
+    {
+      url: "https://via.placeholder.com/85",
+      name: "Amy",
+      rating: "9",
+      date: "09/03/20",
+      pay: "$19/hr",
+      text:
+        "I code well. I code well. I code well. I code well. I code well. I code well.",
+      userID: 1,
+      businessID: 1,
+    },
+    {
+      url: "https://via.placeholder.com/85",
+      name: "Mai",
+      rating: "9",
+      date: "09/03/20",
+      pay: "$19/hr",
+      text: "I code well.",
+      userID: 2,
+      businessID: 1,
+    },
+    {
+      url: "https://via.placeholder.com/85",
+      name: "Jay",
+      rating: "9",
+      date: "09/03/20",
+      pay: "$19/hr",
+      text: "I code well.",
+      userID: 3,
+      businessID: 1,
+    },
+    {
+      url: "https://via.placeholder.com/85",
+      name: "Thomas",
+      rating: "9",
+      date: "09/03/20",
+      pay: "$19/hr",
+      text: "I code well.",
+      userID: 4,
+      businessID: 1,
+    },
+  ];
+
+  const offers = myOffers.map((item) => {
+    return (
+      <BusinessCardItem
+        date={item.date}
+        rating={item.rating}
+        pay={item.pay}
+        text={item.text}
+        freelancerID={item.userID}
+        businessID={item.businessID}
+        history={props.history}
+        freelancerName={item.name}
+        freelancerURL={item.url}
+      />
+    );
+  });
 
   return (
-    <section className="business-offers-container">
+    <main className="business-offers-container">
       <Header />
-      <arcticle className="myOffers-container">
-        {offers}
-      </arcticle>
-    </section>
+      <h1 className="offer-header">{context.user.nickname}'s Offers</h1>
+      <section className="offers-container">{offers}</section>
+    </main>
   );
 }
 
--- src/Routes/Business/BusinessOffers/Components/BusinessCardItem.js
+++ src/Routes/Business/BusinessOffers/Components/BusinessCardItem.js
@@ -2,31 +2,42 @@ import React, { useContext } from "react";
 import ProfilePic from "../../../../Components/Utilities/ProfilePic/ProfilePic";
 import SmallButton from "../../../../Components/Utilities/SmallButton/SmallButton";
 import AppContext from "../../../../AppContext";
+import "./BusinessCard.css";
 
 function BusinessCardItem(props) {
   const context = useContext(AppContext);
   return (
     <article className="offer-item">
-      <ProfilePic imgSrc={props.src} imgAlt={props.name} />
-      <h2 className="offer-name">{props.name}</h2>
-      <p className="offer-rating">{props.rating}</p>
-      <p className="offer-date">{props.date}</p>
-      <p className="offer-pay">Pay rate:{props.pay}</p>
+      <h2 className="offer-name">{props.freelancerName}</h2>
+      <section className="left-card">
+        <ProfilePic
+          className="offer-pic"
+          imgSrc={props.freelancerURL}
+          imgAlt={props.freelancerName}
+        />
+      </section>
+      <section className="right-card">
+        <p className="offer-date">{props.date}</p>
+        <p className="offer-pay">Pay rate:{props.pay}</p>
+      </section>
       <p className="offer-job-desc">{props.text}</p>
-      <SmallButton
-        className="btn"
-        id="cards-message"
-        buttonStyle="btn-outline"
-        buttonSize="btn-large"
-        type="button"
-        onClick={() =>
-          props.history.push(
-            `/Messaging/${context.user.id}/${props.freelancerID}`
-          )
-        }
-      >
-        <i className="fas fa-comments"></i>
-      </SmallButton>
+      <section className="bottom-card">
+        <p className="offer-rating">Rating {props.rating}</p>
+        <SmallButton
+          className="btn"
+          id="cards-message"
+          buttonStyle="btn-outline"
+          buttonSize="btn-large"
+          type="button"
+          onClick={() =>
+            props.history.push(
+              `/Messaging/${context.user.id}/${props.freelancerID}`
+            )
+          }
+        >
+          <i className="fas fa-comments"></i>
+        </SmallButton>
+      </section>
     </article>
   );
 }
